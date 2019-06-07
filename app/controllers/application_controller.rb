0a30595c2db078e81ee4fd6e64f834b32661e664
onyi <onyicho@gmail.com>
2019-06-07 14:30:36 -0700

    Add additional methods for application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   def login(user)
     session[:session_token] = user.reset_session_token!
   end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login?
+    redirect_to new_session_url if !current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
 end
