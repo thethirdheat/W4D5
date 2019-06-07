f422c93bd96680d9bcaeef2301ff7fc710db318d
onyi <onyicho@gmail.com>
2019-06-07 15:32:58 -0700

    Add rspec for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      redirect_to goals_url
+    else
+      flash.now[:errors] = ["Wrong credentials!"]
+      render :new 
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to new_session_url
+  end
+
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
+  describe '#login' do
+
+    it 'Login a user' do
+      User.create(username: 'helloworl', password: 'password')
+      post :create, params: { user: { username: "helloworl", password: "password" } }
+      # expect(session[:session_token]).to eq(user.session_token) # do it later if have time
+      expect(response).to redirect_to(goals_url)
+      # expect(response.to_s).to include("session_token")
+    end
+
+    it 'With wrong param, it should redirect to login page again' do
+      post :create, params: { user: { username: "helloworl", password: "123456" } }
+      expect(response).to render_template("new")
+    end
+
+  end
 end
