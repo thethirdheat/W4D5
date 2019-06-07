6181525eae47c5de5ffb775c1876bbcc56710d3c
onyi <onyicho@gmail.com>
2019-06-07 13:37:58 -0700

    Fix typo in User model validation

--- db/migrate/20190607191424_create_users.rb
+++ db/migrate/20190607191424_create_users.rb
@@ -7,7 +7,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
-    add_index :users,  :session_token, unique: true
+    add_index :users, :session_token, unique: true
     add_index :users, :username, unique: true
   end
 end
